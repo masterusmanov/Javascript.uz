70c05e5c1d4d3f3404bb49734f3196922272889b
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2025-04-04 11:33:24 +0500

    Finish internal navbar

---
+++ src/components/InternalNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="grid md:flex items-center justify-between gap-[12px] border-b-2">
+        <router-link to="" class="linkactiv font-[700] pb-[10px] lg:pb-[22px]">{{$t('internal_navbar.navone.one')}}
+            <p class="text-[15px]">{{$t('internal_navbar.navone.two')}}</p>
+        </router-link>
+         <router-link to="" class="linkactiv font-[700] pb-[10px] lg:pb-[22px]">{{$t('internal_navbar.navtwo.one')}}
+            <p class="text-[15px]">{{$t('internal_navbar.navtwo.two')}}</p>
+        </router-link>
+         <router-link to="" class="linkactiv font-[700] pb-[10px] lg:pb-[22px]">{{$t('internal_navbar.navthree.one')}}
+            <p class="text-[15px]">{{$t('internal_navbar.navthree.two')}}</p>
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+    router-link-exact-active {
+        color: #C4433A;
+        border-bottom: 2px solid #C4433A;
+    }
+    .router-link-exact-active:hover {
+        color: #C4433A;
+        border-bottom: 2px solid #C4433A;
+      }
+      
+</style>
--- src/views/Mainpage/Mainpage.vue
+++ src/views/Mainpage/Mainpage.vue
@@ -1,23 +1,31 @@
 <template>
-    <div class="w-[80%] mx-auto py-[50px] md:flex items-center">
-        <div class="md:w-[50%]">
-            <h1 class="text-[28px] leading-[32px] font-bold md:text-[32px] md:leading-[40px]">{{$t('mainpage.title')}}</h1>
-            <p class="py-[15px] md:py-[25px] text-[18px] md:text-[20px] leading-[24px] font-[700]">{{$t('mainpage.desc')}}</p>
+    <div class="px-[10px] md:px-0 md:w-[80%] mx-auto py-[50px]">
+        <div class="w-[100%] md:flex items-start">
+            <div class="md:w-[60%]">
+                <h1 class="text-[28px] leading-[32px] font-bold md:text-[32px] md:leading-[40px]">{{$t('mainpage.title')}}</h1>
+                <p class="py-[15px] md:py-[25px] text-justify text-[18px] md:text-[20px] leading-[32px] font-[700]">{{$t('mainpage.desc')}}</p>
             <div>
                 <input type="text" name="tutorial" :placeholder="$t('mainpage.tutor')" class="w-[70%] border border-[#F5F2F1] rounded-tl-[8px] rounded-bl-[8px] p-[10px] bg-white dark:bg-gray-800 text-gray-900 dark:text-white">
-                <button class="px-[20px] py-[10px] bg-[#4F9E72] border-2 border-[#4F9E72] rounded-tr-[8px] rounded-br-[8px]">{{$t('mainpage.btn')}}</button>
+                <button class="px-[20px] py-[10px] bg-[#4F9E72] text-[#F5F2F1] font-[700] border-2 border-[#4F9E72] rounded-tr-[8px] rounded-br-[8px]">{{$t('mainpage.btn')}}</button>
             </div>
         </div>
-        <div class="md:w-[50%] md:grid">
+        <div class="md:w-[40%] mt-[50px]">
             <p class="text-right">{{$t('mainpage.updatedate')}} {{new Date().toLocaleDateString("uz-UZ")}}</p>
-            <div>
-                <button class=""></button>
-            </div>
+            <router-link to="#" class="w-[150px] flex mt-[30px] items-center gap-[12px] mx-auto text-[#C4433A] bg-[#F7F4F3] hover:bg-[#B2B0AF] md:text-[18px] font-[700] py-[12px] px-[30px] rounded-[8px]">{{$t('mainpage.social')}} <i class='bx bxl-telegram text-[24px]'></i></router-link>
         </div>
+        </div>
+        <div class="pt-[50px]">
+            <h3 class="font-[900] text-[24px] leading-[32px]">{{$t('mainpage.titletwo')}}</h3>
+            <p class="font-[700] my-[25px] text-justify">{{$t('mainpage.desctwo')}}</p>
+        </div>
+    </div>
+    <div class="px-[10px] md:px-0 md:w-[80%] mx-auto">
+        <InternalNavbar/>
     </div>
 </template>
 
 <script setup>
+import InternalNavbar from '../../components/InternalNavbar.vue';
 </script>
 
 <style lang="scss" scoped>
